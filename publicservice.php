06486ced1e5a9e52e9f26032446262921aed23b2
Iftekhar Ul Islam <136377016+iftekharifty12@users.noreply.github.com>
2024-03-03 15:34:41 +0600

    Add files via upload

---
+++ publicservice.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/94ffa93c29.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include("templete/header.php")?>
+    <div class="container grid text-center">
+        <h3>Empowering Urban Living, Enhancing Safety</h3>
+        <p>At UrbanEase, we believe in fostering vibrant communities by prioritizing your safety and well-being. Our Public Services are designed to provide quick access to essential resources, ensuring a secure and connected urban experience.</p>
+        
+        <h2>Public Service Overview</h2>
+
+        <button class="btn color btn-block mt-2">Police Assistance</button>
+        <button class="btn color btn-block mt-2">Fire Service</button>
+        <button class="btn color btn-block mt-2">Medical Service</button>
+        <button class="btn color btn-block mt-2">Transportation</button>
+        
+    </div>
+
+
+
+
+
+
+    <?php include("templete/footer.php")?>
+
+
+
+    <script src="https://kit.fontawesome.com/94ffa93c29.js" crossorigin="anonymous"></script>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
